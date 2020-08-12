f935c7af4e2784af08bf501d7caa399e3296e123
Anurag <mailmeanurag2@gmail.com>
2020-08-12 16:40:05 +0530

    heroku fix 3

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', (req,res) => {
-    res.sendFile(path.join(__dirname + 'build' + 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT);

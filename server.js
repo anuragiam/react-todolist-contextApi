a4d9a6d475beca1c8f77d45503f4ae1c2ab58c4c
Anurag <mailmeanurag2@gmail.com>
2020-08-12 16:39:03 +0530

    heroku fix 2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || '3001';
 
 app.use(favicon(__dirname + 'build/favicon.ico'));
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirnam, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', (req,res) => {
     res.sendFile(path.join(__dirname + 'build' + 'index.html'));

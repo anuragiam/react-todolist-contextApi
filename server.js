e01eabbd3b9a7bdd5ab19d9897d5911230b8c597
Anurag <mailmeanurag2@gmail.com>
2020-08-12 16:57:27 +0530

    corrected path to favicon

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 const PORT = process.env.PORT || '8080';
 
-app.use(favicon(__dirname + 'build/favicon.ico'));
+app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 

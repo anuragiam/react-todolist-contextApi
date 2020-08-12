dc1ca0c66de125aec5bbfd6200962ac7cd3c1b2c
Anurag <mailmeanurag2@gmail.com>
2020-08-12 16:51:44 +0530

    changed default serve port to 8080

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || '3001';
+const PORT = process.env.PORT || '8080';
 
 app.use(favicon(__dirname + 'build/favicon.ico'));
 app.use(express.static(__dirname));
